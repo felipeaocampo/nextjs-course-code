24f2acafeb480464408bb71ebaa818919fd4dd4c
Felipe Ocampo <felipe.aocampo@gmail.com>
2023-08-28 00:15:59 -0700

    adds dynamic paths

---
+++ pages/clients/[id]/[clientprojectid].js
@@ -0,0 +1,9 @@
+const SelectedClientProjectPage = () => {
+  return (
+    <div>
+      <h1>The project Page for a specific Project for a selected client</h1>
+    </div>
+  );
+};
+
+export default SelectedClientProjectPage;
---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,9 @@
+const ClientProjectsPage = () => {
+  return (
+    <div>
+      <h1>The Projects of a specific client!</h1>
+    </div>
+  );
+};
+
+export default ClientProjectsPage;
